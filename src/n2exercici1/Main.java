9a5e3631eec2390072742247b0f4f2e08d94b4ff
Alex Fernandez <alexfernandez@Sergios-MacBook-Pro.local>
2022-07-07 13:06:46 +0200

    mejoras propuestas por Joan

--- src/n2exercici1/Main.java
+++ src/n2exercici1/Main.java
@@ -10,13 +10,12 @@ public class Main {
 		HashSet<Restaurant> restaurantes = new HashSet<Restaurant>();
 		Restaurant restaurant1 = new Restaurant("Can Roca",10);
 		Restaurant restaurant2 = new Restaurant("Can Roca",9);
-		//Condicion para añadir restaurantes en HashSet
-		if (restaurant1.getNom().equals(restaurant2.getNom()) && (restaurant1.getPuntuacio()==restaurant2.getPuntuacio())) {
-			System.out.println("No podemos añadir restaurantes con el mismo nombre y la misma puntuacion");
-		} else {
+		Restaurant restaurant3 = new Restaurant("Can Roca",9);
+		
 		restaurantes.add(restaurant1);
 		restaurantes.add(restaurant2);
-		}
+		restaurantes.add(restaurant3);
+		
 		//Comprobamos tamaño del hashMap para saber si funciona correctamente
 		System.out.println(restaurantes.size());
 	}
--- src/n2exercici1/Restaurant.java
+++ src/n2exercici1/Restaurant.java
@@ -1,5 +1,7 @@
 package n2exercici1;
 
+import java.util.Objects;
+
 public class Restaurant implements Comparable<Restaurant> {
 	// Atributos
 	private String nom;
@@ -26,6 +28,24 @@ public class Restaurant implements Comparable<Restaurant> {
 
 	public void setPuntuacio(int puntuacio) {
 		this.puntuacio = puntuacio;
+	}
+
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(nom, puntuacio);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Restaurant other = (Restaurant) obj;
+		return Objects.equals(nom, other.nom) && puntuacio == other.puntuacio;
 	}
 
 	@Override
